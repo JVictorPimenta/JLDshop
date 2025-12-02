c2ebd503d0663ff9900789db14181d157334e5f4
Danilloxkl <danillocaua31@gmail.com>
2025-12-02 11:05:46 -0300

    papa e caca

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import ProductDetail from './pages/ProductDetail'
@@ -8,19 +9,27 @@ import Admin from './pages/Admin'
 import Login from './pages/Login'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PaginaDePagamento from './pages/PaginaDePagamento'
+import Confirmacao from './pages/Confirmacao'   // ✅ NOVO IMPORT
+import TesteSteps from './pages/TesteSteps';
 
 export default function App(){
   return (
     <div className="app-container">
       <Header />
+
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/products" element={<ProductList/>} />
-        <Route path="/products/:id" element={<ProductDetail/>} />
-        <Route path="/cart" element={<CartPage/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/admin/*" element={<Admin/>} />
+        <Route path="/teste-steps" element={<TesteSteps />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/pagamento" element={<PaginaDePagamento />} />
+        <Route path="/confirmacao" element={<Confirmacao />} />   {/* ✅ NOVA ROTA */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/*" element={<Admin />} />
       </Routes>
+
       <Footer />
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,24 @@
-import ReactDom from 'react-dom/client'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import 'primereact/resources/themes/saga-blue/theme.css'
+
+import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+
 import './App.css'
+
 import { CartProvider } from './contexts/CartContext'
 import { AuthProvider } from './contexts/AuthContext'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <CartProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </CartProvider>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <CartProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CartProvider>
+  </AuthProvider>
 )
